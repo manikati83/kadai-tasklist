862a8d770d9d2d89a45eb5ed43c6a981ab661eac
kati.mani <tesgit279@gmail.com>
2021-01-15 13:00:34 +0000

    tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
     before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :require_user_logged_in, only: [:index, :show, :new, :create, :update, :destroy]
+    
     
     def index
         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -50,7 +51,10 @@ class TasksController < ApplicationController
     private
     
     def set_task
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @tasks
+            redirect_to root_url
+        end
     end
 
     def task_params
